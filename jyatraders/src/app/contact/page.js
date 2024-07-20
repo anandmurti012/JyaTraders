1713b790067cbd0dcae48eea3205e2a7f4b7912e
anandmurti012 <anand.raj431@gmail.com>
2024-07-20 22:19:15 +0530

    updated contact <a> tag

--- jyatraders/src/app/contact/page.js
+++ jyatraders/src/app/contact/page.js
@@ -13,9 +13,22 @@ export default function ContactSection() {
                     <div className="col-lg-5">
                         <div className="">
                             <div className="section-title-two white-title mb-20 tg-heading-subheading animation-style3">
-                                <h5 style={{fontSize:'25px'}} className="title tg-element-title"><strong style={{color:'#5682e8', fontSize:'20px'}}>Address:</strong><br></br>Chandni Chowk Bishnupur Begusarai Bihar 851129</h5><br></br>
-                                <h5 style={{fontSize:'25px'}} className="title tg-element-title"><strong style={{color:'#5682e8', fontSize:'20px'}}>Phone:</strong><br></br><a href="tel:07856000468">+91 7856000468</a></h5><br></br>
-                                <h5 style={{fontSize:'25px'}} className="title tg-element-title"><strong style={{color:'#5682e8', fontSize:'20px'}}>Email:</strong><br></br><a href="mailto:bookings@wheelzcarrentals.com">jyatrades@gmail.com</a></h5>
+                                <h5 style={{ fontSize: '25px' }} className="title tg-element-title"><strong style={{ color: '#5682e8', fontSize: '20px' }}>Address:</strong><br></br>Chandni Chowk Bishnupur Begusarai Bihar 851129</h5><br></br>
+                                <h5 style={{ fontSize: '25px' }} className="title tg-element-title">
+                                    <strong style={{ color: '#5682e8', fontSize: '20px' }}>Phone:</strong>
+                                    <br />
+                                    <a href="tel:07856000468" style={{ textDecoration: 'underline', color: 'inherit' }}>
+                                        +91 7856000468
+                                    </a>
+                                </h5>
+                                <br />
+                                <h5 style={{ fontSize: '25px' }} className="title tg-element-title">
+                                    <strong style={{ color: '#5682e8', fontSize: '20px' }}>Email:</strong>
+                                    <br />
+                                    <a href="mailto:bookings@wheelzcarrentals.com" style={{ textDecoration: 'underline', color: 'inherit' }}>
+                                        jyatrades@gmail.com
+                                    </a>
+                                </h5>
                             </div>
                             {/* <p>Morem ipsum dolor sit amet, consectetur adipiscing elita florai psum dolor sit amet, consecteture.Borem.</p> */}
                             {/* <VideoPopup style={2} /> */}
@@ -23,31 +36,31 @@ export default function ContactSection() {
                     </div>
                     <div className="col-lg-7">
                         {/* <div className="skill-wrap wow fadeInRight" data-wow-delay=".2s">                      */}
-                            <div className=" col-lg-12 col-md-8 block-9 mb-md-5">
-                                <form className={`bg-light p-5 contact-form ${styles.contactForm}`} action="https://api.web3forms.com/submit" method="POST">
-                                    <input type="hidden" name="access_key" value="a05d4939-0a3f-4a3a-a73d-682b2e3daf29" />
+                        <div className=" col-lg-12 col-md-8 block-9 mb-md-5">
+                            <form className={`bg-light p-5 contact-form ${styles.contactForm}`} action="https://api.web3forms.com/submit" method="POST">
+                                <input type="hidden" name="access_key" value="a05d4939-0a3f-4a3a-a73d-682b2e3daf29" />
 
-                                    <div className="form-group">
-                                        <input name="name" type="text" className={`form-control ${styles.formControl}`} placeholder="Your Name" required />
-                                    </div>
+                                <div className="form-group">
+                                    <input name="name" type="text" className={`form-control ${styles.formControl}`} placeholder="Your Name" required />
+                                </div>
 
-                                    <div className="form-group">
-                                        <input name="phone" type="number" className={`form-control ${styles.formControl}`} placeholder="Your Contact" required />
-                                    </div>
+                                <div className="form-group">
+                                    <input name="phone" type="number" className={`form-control ${styles.formControl}`} placeholder="Your Contact" required />
+                                </div>
 
-                                    <div className="form-group">
-                                        <input name="email" type="email" className={`form-control ${styles.formControl}`} placeholder="Your Email" required />
-                                    </div>
+                                <div className="form-group">
+                                    <input name="email" type="email" className={`form-control ${styles.formControl}`} placeholder="Your Email" required />
+                                </div>
 
-                                    <div className="form-group">
-                                        <textarea name="message" cols="30" rows="4" className={`form-control ${styles.formControl}`} placeholder="Message" required></textarea>
-                                    </div>
+                                <div className="form-group">
+                                    <textarea name="message" cols="30" rows="4" className={`form-control ${styles.formControl}`} placeholder="Message" required></textarea>
+                                </div>
 
-                                    <div>
-                                        <button className={`btn btn-primary ${styles.submitButton}`} type="submit">Submit Form</button>
-                                    </div>
-                                </form>
-                            </div>
+                                <div>
+                                    <button className={`btn btn-primary ${styles.submitButton}`} type="submit">Submit Form</button>
+                                </div>
+                            </form>
+                        </div>
                         {/* </div> */}
                     </div>
                 </div>
